a6bc698c3add86b8d3eb1c0580c8f54b732e7f4e
trey_weathers <treychelle.weathers2017@gmail.com>
2023-10-17 18:51:37 -0400

    Course 4 progress

---
+++ 1_Pandas/lab_1/setup-env 2.sh
@@ -0,0 +1,3 @@
+mkdir -p envs
+python -m venv envs/starter-env
+source `pwd`/envs/starter-env/bin/activate
---
+++ 1_Pandas/lab_4/complete_Expression_Statements 2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Expression Statements
+# 
+# ## Echo a number.
+#  Write a number in the code cell below, then press the run button above. Notice the number is echoed after the line.
+#   
+
+# In[21]:
+
+
+4
+
+
+# ## Addition
+#  Now write a simple addition statement using the `+` sign and two numbers. Press the run button again to see the result.
+
+# In[22]:
+
+
+2 + 8
+
+
+# ## Mathematical expressions
+#  Try writing other mathematical expressions using `-`, `+`, `*`, `/` and `**`. You can group expressions by using parenthesis. You modify the example below or write your own.
+
+# In[23]:
+
+
+(12 + 3**40) * (4 - 0.22)/10000 * (1000 - 100)
+
+
+# ## Echo a string (word)
+#  Type the word `"Hello"` with the quotes and press run.
+
+# In[24]:
+
+
+"Hello"
+
+
+# Notice that it is echoed below the cell, just as the numbers above. This will be the case for any expression statement in an interactive enviroment.
